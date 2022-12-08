74b2e315800047e305403215ad0d0ab4945ae27e
Patricia Palomino <pattyandre.28@gmail.com>
2022-12-08 00:45:21 -0500

    integration api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,38 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useEffect, useState } from "react";
 import { AppCard } from "../../components";
+// import { GetAccounts } from "../../service/accounts";
 import "./index.scss";
 
-interface HomeProps {}
+interface AccountProps {
+  id: number;
+  balance: number;
+  creditline: string | number | "";
+  name: string;
+  status: string;
+  type: string;
+}
 
-export const Home: FC<HomeProps> = () => {
+export const Home = () => {
+  const [accounts, setAccounts] = useState([]);
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      await getAccounts();
+    })();
+  }, []);
+
+  const getAccounts = async () => {
+    fetch(
+      "https://my-json-server.typicode.com/racordovaindracompany/accounts/accounts"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setAccounts(data.filter((item: any) => item.type === "ACCOUNT"));
+        setCards(data.filter((item: any) => item.type === "CREDIT_CARD"));
+      });
+  };
+
   return (
     <section>
       <AppCard>
@@ -13,28 +41,43 @@ export const Home: FC<HomeProps> = () => {
         </h1>
       </AppCard>
       <AppCard>
-        <h2>CUENTAS</h2>
         <div>
-          <div className="card-detail">
-            <p className="card-detail__title">
-              Cuenta fácil <br />
-              <span className="card-detail__title__account-number">*3970</span>
-            </p>
-            <p className="card-detail__title__balance">
-              1500 <br />
-              <span>Saldo disponible</span>
-            </p>
-          </div>
-          <div className="card-detail">
-            <p className="card-detail__title">
-              Cuenta fácil <br />
-              <span className="card-detail__title__account-number">*3970</span>
-            </p>
-            <p className="card-detail__title__balance">
-              1500 <br />
-              <span>Saldo disponible</span>
-            </p>
-          </div>
+          <h2>CUENTAS</h2>
+          {accounts.map((account: AccountProps) => (
+            <div className="card-detail" key={account.id}>
+              <p className="card-detail__title">
+                {account.name} <br />
+                {/* Use the ID as account number */}
+                <span className="card-detail__title__account-number">
+                  *{account.id}
+                </span>
+              </p>
+              <p className="card-detail__title__balance">
+                S/ {account.balance} <br />
+                <span>{account.creditline === "" && "Saldo disponible"}</span>
+              </p>
+            </div>
+          ))}
+        </div>
+      </AppCard>
+      <AppCard>
+        <div>
+          <h2>TARJETAS</h2>
+          {cards.map((card: AccountProps) => (
+            <div className="card-detail" key={card.id}>
+              <p className="card-detail__title">
+                {card.name} <br />
+                {/* Use the ID as account number */}
+                <span className="card-detail__title__account-number">
+                  *{card.id}
+                </span>
+              </p>
+              <p className="card-detail__title__balance">
+                S/ {card.balance} <br />
+                <span>{card.creditline !== "" && "Línea de crédito"}</span>
+              </p>
+            </div>
+          ))}
         </div>
       </AppCard>
     </section>
